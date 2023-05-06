391cfa4fd63ca6e3bd67c523e1a153b7b7764dc4
Noaman <noamangg@gmail.com>
2023-05-06 17:59:50 +0300

    some inhancment

---
+++ Portfolio- My Main/main.js
@@ -0,0 +1,16 @@
+let main = document.querySelector("main");
+let aside = document.querySelector("aside");
+let menuIcon = document.querySelector("#minu-icon");
+
+menuIcon.onclick = function (e) {
+  if (aside.style.display === "none") aside.style.display = "block";
+  else aside.style.display = "none";
+};
+if (main.clientWidth > 1024) {
+}
+
+main.onclick = function (e) {
+  if (main.clientWidth <= 1024) {
+    if (aside.style.display === "block") aside.style.display = "none";
+  }
+};
