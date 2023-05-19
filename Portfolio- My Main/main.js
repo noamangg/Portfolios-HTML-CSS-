0838c553ecb798f0f1683b22ce1ed0afcc25c26a
Noaman <noamangg@gmail.com>
2023-05-19 21:23:14 +0300

    add the copywriting

--- Portfolio- My Main/main.js
+++ Portfolio- My Main/main.js
@@ -1,6 +1,7 @@
 let main = document.querySelector("main");
 let aside = document.querySelector("aside");
 let menuIcon = document.querySelector("#minu-icon");
+aside.style.display === "block"
 
 menuIcon.onclick = function (e) {
   if (aside.style.display === "none") aside.style.display = "block";
@@ -8,9 +9,9 @@ menuIcon.onclick = function (e) {
 };
 if (main.clientWidth > 1024) {
 }
-
 main.onclick = function (e) {
   if (main.clientWidth <= 1024) {
     if (aside.style.display === "block") aside.style.display = "none";
   }
 };
+
