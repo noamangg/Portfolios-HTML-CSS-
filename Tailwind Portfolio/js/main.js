082f011b674d2b63001ff3846587c93e3509c645
Noaman <noamangg@gmail.com>
2023-06-17 01:39:01 +0300

    ^-^

--- Tailwind Portfolio/js/main.js
+++ Tailwind Portfolio/js/main.js
@@ -1,18 +1,13 @@
 let modeIcons = document.querySelectorAll(".mode-icon");
 
-console.log(modeIcons);
-
-if(localStorage.mode === "dark") {
+if (localStorage.mode === "dark") {
     document.documentElement.classList.toggle(localStorage.mode);
     document.querySelector(`[data-mode="dark"]`).classList.add("display-none");
     document.querySelector(`[data-mode="light"]`).classList.remove("display-none");
-
 }
 modeIcons.forEach(i => {
-    console.log(i);
-    i.addEventListener('click', function(e) {
-        console.log(e.target);
-        if(e.target.dataset.mode === "dark") {
+    i.addEventListener('click', function (e) {
+        if (e.target.dataset.mode === "dark") {
             localStorage.mode = "dark";
             document.querySelector(`[data-mode="light"]`).classList.remove("display-none");
         }
@@ -23,10 +18,6 @@ modeIcons.forEach(i => {
         e.target.classList.add("display-none");
         document.documentElement.classList.toggle("dark");
 
-        
-        
     })
 })
-
-console.log("he")
 
